3f009d86d67b66e556dc8a01af110223d4a15304
Luis Impieri <128927595+Luis-Impieri@users.noreply.github.com>
2023-04-17 14:51:56 -0300

    Calculadora atualizada

--- CalculadoraBasica.py
+++ CalculadoraBasica.py
@@ -1,15 +1,33 @@
-A = int(input('digite um numero'))
+import math
+
+A = float(input('digite um numero'))
 conta = input('digite o símbolo para realizar a operação')
-B = int(input('digite o segundo numero'))
+B = float(input('digite o segundo numero'))
 if conta == '+':
   print(A + B)
 elif conta == '-':
   print(A - B)
 elif conta == '/':
-  print(A / B)
+  if B == 0:
+    print('Não é possível dividir po Zero')
+  else:
+    print(A / B)
 elif conta == '*':
-  print(A * B)
+  if B == 0:
+    print('Não é possível multiplicar por Zero')
+  else:
+    print(A * B)
 elif conta == '**':
   print(A**B)
+
+elif conta == "//":
+  print(math.sqrt(A))
+
+elif conta == "log":
+  print(math.log(A, B))
+
+elif conta == "$":
+  print("sen ",math.sin(A), "cos ",math.cos(A), "tan ",math.tan(A))
+
 else:
   print('Digite uma operação válida')
